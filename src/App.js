8a7394e174847f2a1fbd029ab97d514a22d2a399
JoeTsang <53040343+YauKin@users.noreply.github.com>
2022-05-13 10:25:49 +0800

    Testing for commit code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import "./styles.css";
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 export default App;
